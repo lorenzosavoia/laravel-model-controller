4918923bcdc8354b16d547522dc41c3967efb7fd
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-02-17 17:15:13 +0100

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class PageController extends Controller
+{   
+    public function index()
+    {
+        $movies = Movie::all();
+        $data= ['movie' => $movies];
+        return view('home', $data);    
+    }
+
+    // public function movie()
+    // {
+    //     return view('home');
+    // }
+}
